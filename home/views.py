4e568755ddb9411c71e9146bd315a61c3ba866a2
Roohan <roohank8@gmail.com>
2022-09-25 03:54:38 +0500

    First

---
+++ home/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+    # return HttpResponse('This is response')
+    # context = {
+    #     'variable' : 'This is sent'
+    # }
+def about(request):
+    return render(request, 'about.html')
+    # return HttpResponse('This is about page')
+
+def services(request):
+    return render(request, 'services.html')
+    # return HttpResponse('This is services page')
+
+def contact(request):
+    return render(request, 'contact.html')
+    # return HttpResponse('This is contact page')
