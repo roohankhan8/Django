1f5038c6b7da70e1e6960ee41035ef5d589224ac
Roohan <roohank8@gmail.com>
2022-09-26 02:10:58 +0500

    Third

--- home/views.py
+++ home/views.py
@@ -1,8 +1,17 @@
+import imp
+from unicodedata import name
 from django.shortcuts import render, HttpResponse
+#Import Date Time
+from datetime import datetime
+#Import Class
+from home.models import Contact
+#Import Messages
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+    # messages.success(request, 'This is success message')
     # return HttpResponse('This is response')
     # context = {
     #     'variable' : 'This is sent'
@@ -16,5 +25,13 @@ def services(request):
     # return HttpResponse('This is services page')
 
 def contact(request):
+    if request.method == 'POST':
+            name = request.POST.get('name'),
+            email = request.POST.get('email'),
+            number = request.POST.get('number'),
+            contact = Contact(name=name, email=email, number=number, date=datetime .today())
+            contact.save()
+            messages.success(request, 'Profile details updated!')
+    
     return render(request, 'contact.html')
     # return HttpResponse('This is contact page')
